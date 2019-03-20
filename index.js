9f1debf81845c231599b69116cc4ee3ab03454c4
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-03-20 16:07:32 -0600

    Finished with MVP

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const server = require('./server');
+const port = 3200;
+
+server.listen(port, () => {
+    console.log(`* Server Running on http://localhost:${port} *`);
+});
+
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
+
+const postsRouter = require('./routers/posts-router');
+const usersRouter = require('./routers/users-router');
+
+const server = express();
+
+// global middleware
+function checkForCapital(req, res, next) {
+    if((req.method === "POST" || req.method === "PUT") && req.body.name){
+        req.body.name = req.body.name.charAt(0).toUpperCase() + req.body.name.slice(1);
+    }
+    next();
+}
+
+function teamNamer(req, res, next) {
+    req.team = 'Web XVII'; // middleware can modify the request
+    next(); // go ahead and execute the next middleware/route handler
+}
+
+server.use(express.json()); // built-in, no need to yarn add
+server.use(helmet()); // third party, need to npm install or yarn add it
+server.use(checkForCapital);
+
+// routing
+server.use('/posts', postsRouter);
+server.use('/users', usersRouter);
+
+// route handlers ARE middleware
+server.get('/', restricted, (req, res) => {
+    res.send(`
+    <h2>Lambda Hubs API</h2>
+    <p>Welcome ${req.team} to the Lambda Hubs API</p>
+    `);
+});
+
+function restricted(req, res, next) {
+    const password = req.headers.password;
+    
+    if (password === 'mellon') {
+        next();
+    } else {
+        res.status(401).send('You shall not pass Balrog!');
+    }
+}
+
+module.exports = server;
